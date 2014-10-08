6df2340f73abddedd6fef1c1450b2f7ad067bde7
Marek Kasztelnik <mkasztelnik@gmail.com>
2014-10-08 14:56:10 +0200

    set atmosphere sidekiq config

--- config/initializers/atmosphere.rb
+++ config/initializers/atmosphere.rb
@@ -4,4 +4,7 @@ Atmosphere.setup do |config|
   config.admin_entites_ext = {
     main_app: [ SecurityProxy, SecurityPolicy ]
   }
+
+  config.redis_url = Air.config.sidekiq.url
+  config.redis_namespace = Air.config.sidekiq.namespace
 end
