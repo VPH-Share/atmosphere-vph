b2c38fb739552737027f42aa9b7a08dcf7bdea09
Marek Kasztelnik <mkasztelnik@gmail.com>
2014-10-17 19:53:08 +0200

    use CachedDelegator to create influxdb client

--- config/initializers/atmosphere.rb
+++ config/initializers/atmosphere.rb
@@ -1,3 +1,5 @@
+require 'atmosphere/cached_delegator'
+
 Atmosphere.setup do |config|
   config.delegation_initconf_key = Air.config.mi_authentication_key
 
@@ -5,14 +7,20 @@ Atmosphere.setup do |config|
     main_app: [ SecurityProxy, SecurityPolicy ]
   }
 
-  config.redis_url = Air.config.sidekiq.url
-  config.redis_namespace = Air.config.sidekiq.namespace
+  config.sidekiq.url = Air.config.sidekiq.url
+  config.sidekiq.namespace = Air.config.sidekiq.namespace
 
-  if Air.config.at_pdp
+  if Air.config['at_pdp']
     config.at_pdp_class = Air.config.at_pdp.constantize
   end
 
   if Air.config['zabbix']
     config.monitoring_client = Atmosphere::Monitoring::ZabbixClient.new
+  end
+
+  if Air.config['influxdb']
+    config.metrics_store = Atmosphere::CachedDelegator.new(60.minutes) do
+      Atmosphere::Monitoring::InfluxdbMetricsStore.new(config['influxdb'])
+    end
   end
 end
