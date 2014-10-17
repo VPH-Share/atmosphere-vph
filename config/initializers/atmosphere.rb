b8d25e76a3f9df541bb5d3a9bfb58391734589de
Marek Kasztelnik <mkasztelnik@gmail.com>
2014-10-17 13:22:29 +0200

    vphshare pdp definition

--- config/initializers/atmosphere.rb
+++ config/initializers/atmosphere.rb
@@ -7,4 +7,8 @@ Atmosphere.setup do |config|
 
   config.redis_url = Air.config.sidekiq.url
   config.redis_namespace = Air.config.sidekiq.namespace
+
+  if Air.config.at_pdp
+    config.at_pdp_class = Air.config.at_pdp.constantize
+  end
 end
