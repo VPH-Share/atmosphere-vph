6ac5db94acc0eff08b0c995a9aa71f603a77861a
Paweł Suder <pawel@suder.info>
2015-01-26 12:11:46 +0100

    Update to latest version, update db migrations

---
+++ db/migrate/20150126110630_create_migration_jobs.atmosphere.rb
@@ -0,0 +1,28 @@
+# This migration comes from atmosphere (originally 20140819113320)
+class CreateMigrationJobs < ActiveRecord::Migration
+  def change
+    create_table :atmosphere_migration_jobs do |t|
+      t.references :appliance_type
+      t.references :compute_site_source
+      t.references :compute_site_destination
+      t.text :status
+
+      t.timestamps
+    end
+
+    add_foreign_key :atmosphere_migration_jobs,
+                    :atmosphere_appliance_types,
+                    column: 'appliance_type_id',
+                    name: 'atmo_mj_at_fk'
+
+    add_foreign_key :atmosphere_migration_jobs,
+                    :atmosphere_compute_sites,
+                    column: 'compute_site_source_id',
+                    name: 'atmo_mj_css_fk'
+
+    add_foreign_key :atmosphere_migration_jobs,
+                    :atmosphere_compute_sites,
+                    column: 'compute_site_destination_id',
+                    name: 'atmo_mj_csd_fk'
+  end
+end
