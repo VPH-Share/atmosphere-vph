f920f507463b300b61dee8e21149b2f678dee894
Marek Kasztelnik <mkasztelnik@gmail.com>
2015-02-25 13:00:20 +0100

    Disable CSRF validation when token API request is made
    See https://github.com/dice-cyfronet/atmosphere/pull/118 for rationale.

--- app/controllers/concerns/atmosphere/api/application_controller_ext.rb
+++ app/controllers/concerns/atmosphere/api/application_controller_ext.rb
@@ -1,3 +1,5 @@
+require 'devise/strategies/token_authenticatable'
+
 module Atmosphere
   module Api
     module ApplicationControllerExt
@@ -5,6 +7,23 @@ module Atmosphere
 
       def delegate_auth
         current_user ? current_user.mi_ticket : nil
+      end
+
+      def token_request?
+        mi_ticket || token
+      end
+
+      private
+
+      def mi_ticket
+        params[Air.config.mi_authentication_key] ||
+          request.headers[Air.config.header_mi_authentication_key]
+      end
+
+      def token
+        params[Devise::Strategies::TokenAuthenticatable.key].presence ||
+          request.headers[Devise::Strategies::TokenAuthenticatable.header_key].
+            presence
       end
     end
   end
